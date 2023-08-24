63f4ae914a1adcffeabd0009037dd19d0c024694
ftbeatzz <last.servers@list.ru>
2023-08-24 15:24:22 +0300

    hw3

--- hw1/script.js
+++ hw1/script.js
@@ -1,40 +1,85 @@
-let a = 10;
-alert(a)
+let password = String(prompt('Введите свой пароль'));
+if (password === 'password123') {
+    console.log('Пароль верный');
+} else {
+    console.log('Пароль неверный');
+}
 
-let year = 2007
-alert(year)
+let c = -2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно')
+}
+let d = 2;
+let e = 36;
 
-let nameCreator = 'Brendan Eich'
-alert(nameCreator)
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно')
+}
 
-let num = 10;
+let a = '2';
+let b = '3';
 
-alert(num + 2);
-alert(num - 2);
-alert(num * 2);
-alert(num / 2);
+alert(Number(a) + Number(b));
 
-let result = 32;
-alert(result);
+let monthNumber = prompt('Введите месяц');
 
-let b = 9;
-let c = 2;
+if (monthNumber === '1') {
+  console.log('Зима');
+} else if (monthNumber === '2') {
+  console.log('Зима');
+} else if (monthNumber === '3') {
+  console.log('Весна');
+} else if (monthNumber === '4') {
+  console.log('Весна');
+} else if (monthNumber === '5') {
+  console.log('Весна');
+} else if (monthNumber === '6') {
+  console.log('Лето');
+} else if (monthNumber === '7') {
+    console.log('Лето');
+} else if (monthNumber === '8') {
+    console.log('Лето');
+  } else if (monthNumber === '9') {
+    console.log('Осень');
+  } else if (monthNumber === '10') {
+    console.log('Осень');
+  } else if (monthNumber === '11') {
+    console.log('Осень');
+  } else if (monthNumber === '12') {
+    console.log('Зима');
+} else {
+  console.log('В году всего 12 месяцев');
+}
 
-alert(b % c);
+let foreverNumber = Number(prompt('Введите любое число'));
 
-let age = prompt('Сколько Вам Лет?');
-alert(age);
+if (foreverNumber % 2 === 0) {
+  alert('Число четное');
+} else {
+  alert('Число не четное')
+}
 
-let user = {
-    age: 28,
-    name: "John",
-    isAdmin: false
-};
-user['city of residence'] = true;
-user.age = 14;
-delete user['city of residence'];
-let info = prompt(`Какую информацию хотите узнать о пользователе? ${user.name}`);
-alert(user[info]);
+let clientOS = prompt("Введите 0, если вы используете iOS, или 1, если вы используете Android:");
+let clientDeviceYear = prompt("Введите год создания вашего устройства:");
 
-let yourName = prompt('Введите пожалуйста Ваше имя');
-alert(`Привет, ${yourName}`);
+if (clientDeviceYear >= 2015) {
+    if (clientOS === 0) {
+        console.log("Установите приложение для iOS по ссылке");
+    } else if (clientOS === 1) {
+        console.log("Установите приложение для Android по ссылке");
+    } else {
+        console.log("Неизвестная операционная система");
+    }
+} else {
+    if (clientOS === 0) {
+        console.log("Установите облегченную версию приложения для iOS по ссылке");
+    } else if (clientOS === 1) {
+        console.log("Установите облегченную версию приложения для Android по ссылке");
+    } else {
+        console.log("Неизвестная операционная система");
+    }
+}
