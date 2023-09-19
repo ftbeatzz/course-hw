f4ac95398e7489d63e1bab6980f81fe11860882a
ftbeatzz <last.servers@list.ru>
2023-09-19 21:36:08 +0300

    hw5

--- hw1/script.js
+++ hw1/script.js
@@ -1,41 +1,108 @@
-let a = 1;
-
-while (a <= 2) {
-  console.log('Привет!');
-  a++;
+function searchMinNumber(a, b) {
+  let min;
+  if (a < b) {
+      min = a;
+  } else {
+      min = b;
+  }
+  return min;
 }
 
-for (let b = 1; b <= 5; b++) {
-  console.log(b);
+console.log(searchMinNumber(6, 10));
+
+function parity() {
+  let result;
+  let qestion = prompt('Введите число');
+  if (qestion % 2 === 0) {
+    result = 'Число четное'
+  } else {
+    result = 'Число нечетное'
+  }
+  return result;
 }
 
-for (let c = 7; c <= 22; c++) {
-  console.log(c);
-}
+function helloAge() {
+  let age = prompt("Сколько вам лет?");
 
-let obj = {
-  'Коля': '200',
-  'Вася': '300',
-  'Петя': '400'
+  if (age < 12) {
+    console.log('Привет, Друг!');
+  } else if (age > 12) {
+    console.log('Добро пожаловать!');
+    }
+}
+helloAge();
+
+
+function checkNumbers(a, b) {
+  let firstNumber = prompt("Введите первое число:");
+  let secondNumber = prompt("Введите второе число:");
+
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+      return 'Одно или оба значения не являются числом';
+  } else {
+      return firstNumber * secondNumber;
+  }
+}
+console.log(checkNumbers());
+
+function squareNumber() {
+  let result = prompt("Введите число:");
+
+  if (isNaN(result)) {
+      return 'Переданный параметр не является числом';
+  } else {
+      let cube = result ** 3;
+      return `${result} в кубе равняется ${cube}`;
+  }
+}
+console.log(squareNumber());
+
+// Это задание не понял от слова совсем, сделал через нейросеть, но смысл остался тем же, ничего не понятно, пытался больше часа - все безуспешно :) 
+
+let circle1 = {
+  radius: 5,
+
+  getArea: function() {
+      return Math.PI * this.radius ** 2;
+  },
+
+  getPerimeter: function() {
+      return 2 * Math.PI * this.radius;
+  }
 };
 
-for (let key in obj) {
-  console.log(`${key} — зарплата ${obj[key]} долларов.`);
-}
+let circle2 = {
+  radius: 8, 
 
-let n = 1000;
-let num = 0;
+  getArea: function() {
+      return Math.PI * this.radius ** 2;
+  },
 
-while (n >= 50) {
-    n /= 2;
-    num++;
-}
+  getPerimeter: function() {
+      return 2 * Math.PI * this.radius;
+  }
+};
 
-console.log("Получившееся число:", n);
-console.log("Количество итераций:", num);
+console.log("Площадь круга circle1:", circle1.getArea());
+console.log("Периметр окружности circle1:", circle1.getPerimeter());
 
-const firstFriday = 5;
+console.log("Площадь круга circle2:", circle2.getArea());
+console.log("Периметр окружности circle2:", circle2.getPerimeter());
 
-for (let day = firstFriday; day <= 31; day += 7) {
-    console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+
+function timeYear() {
+  let month = prompt('Укажите номер месяца')
+  if (month >= 1 && month <= 12) {
+      if (month >= 3 && month <= 5) {
+          return "Весна";
+      } else if (month >= 6 && month <= 8) {
+          return "Лето";
+      } else if (month >= 9 && month <= 11) {
+          return "Осень";
+      } else {
+          return "Зима";
+      }
+  } else {
+      return "Неправильный номер месяца";
+  }
 }
